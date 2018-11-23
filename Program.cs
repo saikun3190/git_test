c9c534967c28fd1b2f4a971664027b7866a511d3
saikun3190 <saikun3190@gmail.com>
2018-11-23 19:57:38 +0900

    3rd commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace git_test
     {
         static void Main(string[] args)
         {
+            string str;
+            str = Console.ReadLine();
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hero");
+
+            
+            int a;
+            //Console.Read();
+            try{
+                str = Console.ReadLine();
+                a = int.Parse(str);
+
+            }catch(Exception ex){
+                Console.WriteLine(ex.Message);
+                str = "error";
+            }
+
+
+            Console.WriteLine(str);
         }
     }
 }
